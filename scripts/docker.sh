62f601bd9509ad7a5316da381a2ce53aa7658854
William Hearn <sylus1984@gmail.com>
2018-02-14 13:31:27 -0500

    feat(docker): Add docker compose

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,8 +5,10 @@ if [[ $DOCKER  =~ true || $DOCKER =~ 1 || $DOCKER =~ yes ]]; then
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     sudo yum-config-manager –enable docker-ce-edge
     sudo yum makecache fast
-    yum -y install docker-ce
+    yum -y install docker-ce python-pip
     systemctl start docker
     systemctl enable docker
+	sudo pip install docker-compose
     sudo usermod -aG docker $(whoami)
+    sudo usermod -aG docker $SSH_USERNAME
 fi
